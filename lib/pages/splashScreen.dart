050959e6c59b3f75c3143c5c1e3f7040e92a5746
Dan Klaver <dklaver15@gmail.com>
2022-11-18 14:10:06 -0600

    Initial creation of the splash screen

---
+++ lib/pages/splashScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class SplashscreenWidget extends StatefulWidget {
+  const SplashscreenWidget({Key? key}) : super(key: key);
+
+  @override
+  _SplashscreenWidgetState createState() => _SplashscreenWidgetState();
+}
+
+class _SplashscreenWidgetState extends State<SplashscreenWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(milliseconds: 1500));
+
+      //context.pushNamed('HomePage');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: const Color(0xFF000000),
+      body: Stack(
+        children: [
+          Image.asset(
+            '../assets/images/mesh-gradient.png',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 1,
+            fit: BoxFit.fill,
+          ),
+          Align(
+            alignment: const AlignmentDirectional(0, 0),
+            child: Image.asset(
+              '../assets/images/iaq_icon_white.png',
+              width: MediaQuery.of(context).size.width * 0.95,
+              height: MediaQuery.of(context).size.height * 0.95,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
