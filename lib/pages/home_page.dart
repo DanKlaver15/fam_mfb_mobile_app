c0e9bab58b1f918ce4725c1ed290a88acdc3960f
Dan Klaver <dklaver15@gmail.com>
2022-11-22 21:47:46 -0600

    Added IAQ infographics and fixed pagination dots

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:app/generated/assets.dart';
+
+import '../components/custom_appbar.dart';
+import '../modals/login_modal.dart';
+import '../modals/registration_modal.dart';
 
 class HomePageWidget extends StatefulWidget {
   const HomePageWidget({Key? key}) : super(key: key);
@@ -17,108 +22,140 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(backgroundColor: Colors.red);
-
     return Scaffold(
-      backgroundColor: const Color(0xFF114E9D),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF000000),
-        automaticallyImplyLeading: false,
-        title: const Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-          child: Text(
-            'My Full Breath',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xFFE5E5E5),
-              fontWeight: FontWeight.w500,
-              fontSize: 24,
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: CustomAppBar(
+          elevation: 30,
+        ),
+      ),
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromRGBO(15, 76, 117, 1),
+                Color.fromRGBO(27, 38, 44, 1),
+              ],
             ),
           ),
-        ),
-        actions: const [],
-        centerTitle: true,
-        elevation: 30,
-      ),
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Align(
-                alignment: const AlignmentDirectional(0, 0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(12, 18, 12, 12),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print('Button pressed ...');
-                      },
-                      child: const Text('Register'),
-                    ),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: const AlignmentDirectional(0, 0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
-                    child: Row(children: const <Widget>[
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey,
-                          thickness: 1,
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
-                        child: Text(
-                          'or',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
+          child: SafeArea(
+            child: GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            12, 18, 12, 12),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 15,
+                            backgroundColor:
+                                const Color.fromRGBO(50, 130, 184, 1),
+                          ),
+                          onPressed: _showRegistrationModal,
+                          child: const Text(
+                            'Register',
+                            style: TextStyle(
+                              fontFamily: Assets.fontsMontserratBlack,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey,
-                          thickness: 1,
-                        ),
-                      ),
-                    ]),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: const AlignmentDirectional(0, 0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
-                    child: ElevatedButton(
-                      style: style,
-                      onPressed: () {
-                        print('Button pressed ...');
-                      },
-                      child: const Text('Sign In'),
                     ),
                   ),
-                ),
-              )
-            ],
+                  Align(
+                    alignment: const AlignmentDirectional(0, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            12, 12, 12, 12),
+                        child: Row(children: const <Widget>[
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey,
+                              thickness: 1,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
+                            child: Text(
+                              'or',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey,
+                              thickness: 1,
+                            ),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: const AlignmentDirectional(0, 0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            12, 12, 12, 12),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            elevation: 15,
+                          ),
+                          onPressed: _showLoginModal,
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontFamily: Assets.fontsMontserratBlack,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  void _showRegistrationModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const RegistrationModal();
+      },
+    );
+  }
+
+  void _showLoginModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const LoginModal();
+      },
     );
   }
 }
